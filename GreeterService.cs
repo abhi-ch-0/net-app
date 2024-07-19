d640f6b53576b6b27d58275b16e301acd44b9344
“abhi” <abhichouhan@proton.me>
2024-07-19 22:00:53 +0530

    remove unnecessary code

--- GreeterService.cs
+++ GreeterService.cs
@@ -1,15 +1,12 @@
 using Grpc.Core;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace NetApp
 {
     public class GreeterService : Greeter.GreeterBase
     {
-        private readonly ILogger<GreeterService> _logger;
-        public GreeterService(ILogger<GreeterService> logger)
+        public GreeterService()
         {
-            _logger = logger;
         }
 
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
