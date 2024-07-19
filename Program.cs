c346dda9bfcd05b860d5eb283516e4943f3795ca
“abhi” <abhichouhan@proton.me>
2024-07-19 17:57:09 +0530

    add grpc

--- Program.cs
+++ Program.cs
@@ -1,14 +1,31 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using NetApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5000, o => o.Protocols =
+        HttpProtocols.Http2);
+});
+
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
-app.UseRouting();
+app.MapGrpcService<GreeterService>();
 
-app.MapControllers();
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
+
+app.MapGet("/", () => "Use a gRPC client to communicate with the gRPC endpoints.");
 
 app.Run();
